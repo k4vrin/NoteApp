1583205d91c502a794c8049ed8eeb3acce4a664d
Mostafa Hosseini <kavrindev@gmail.com>
2022-05-28 23:48:03 +0430

    Setup sql database

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDatabase.kt
@@ -0,0 +1,13 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.data.data_source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+
+
+@Database(entities = [Note::class], version = 1, exportSchema = true)
+abstract class NoteDatabase() : RoomDatabase() {
+
+	abstract val noteDao: NoteDao
+
+}
